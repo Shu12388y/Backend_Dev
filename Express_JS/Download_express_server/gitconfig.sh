869aef21654f2eb1ba3a6b7a745ae21f73c82650
Shu12388y <spl.sp999@gmail.com>
2023-08-07 09:06:02 +0530

    upadte

--- Express_JS/Download_express_server/gitconfig.sh
+++ Express_JS/Download_express_server/gitconfig.sh
@@ -1,4 +1,10 @@
 git add .
 read message
 git commit -m "$message"
-git push
+echo "enter your option"
+read option
+if [ $option -eq 1 ]
+then
+    git push origin main
+else    
+    git push upstream main
