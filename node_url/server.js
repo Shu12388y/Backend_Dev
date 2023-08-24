6c9bd23a17659aae17cff1fd076179da212b1647
Shu12388y <spl.sp999@gmail.com>
2023-08-24 22:06:03 +0530

    file are added

--- node_url/server.js
+++ node_url/server.js
@@ -8,6 +8,14 @@ const PORT=3000 || process.env.PORT
 app.use(express.static('public'))
 
 
+
+app.get("/about",(req,res,next)=>{
+    res.sendFile("./static/about.html",(error)=>{
+        throw error;
+    })
+})
+
+
 app.listen(PORT,()=>{
     console.log("server is on")
 })
