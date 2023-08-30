8ff21e0198d24df18fd753e81614c854f02415a3
Shu12388y <spl.sp999@gmail.com>
2023-08-30 16:09:01 +0530

    added the change file

--- MVC/server.js
+++ MVC/server.js
@@ -1,19 +1,17 @@
-const express=require("express")
-const PORT=3000
-const app=express()
+const express = require("express");
+const PORT = 3000;
+const app = express();
 
-const homePage=require("./routes/homepage")
+const homePage = require("./routes/homepage");
 
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.set("view engine","ejs")
-app.set("views","views")
-
-app.use(express.static('public'))
-app.use(homePage)
-
+app.use(express.static("public/css/style.css"));
+app.use(homePage);
 
 // listen to port express
 
-app.listen(PORT,()=>{
-    console.log("server is on")
-})
+app.listen(PORT, () => {
+  console.log("server is on");
+});
