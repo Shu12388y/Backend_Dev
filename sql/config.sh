289f09adf001d7472eda1298388bbdf2a748f65c
Shu12388y <spl.sp999@gmail.com>
2023-09-02 12:55:22 +0530

    file added

---
+++ sql/config.sh
@@ -0,0 +1,14 @@
+touch server.js
+npm init -y
+npm i express body-parser ejs mysql --save 
+mkdir views
+mkdir controller
+mkdir models
+touch Dockerfile
+cd views
+touch index.ejs
+cd ..
+cd controller
+touch homePage.js
+cd ..
+echo "done ..."
