7e4481f0466f94c4b416e21353158677d24f2302
Shu12388y <spl.sp999@gmail.com>
2023-10-03 05:06:15 +0530

    file added

---
+++ sql_db/model/model.js
@@ -0,0 +1,8 @@
+const db=require("../util/db")
+
+
+class model{
+    static fetch(){
+       return db.execute("SELECT * FROM user")
+    }
+}
--- sql_db/server.js
+++ sql_db/server.js
@@ -1,1 +1,13 @@
-cons
+const express=require("express")
+const app=express()
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server is on")
+})
