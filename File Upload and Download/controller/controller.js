ccbbadeb36907e481a672ee38f75a155a70039ec
Shu12388y <spl.sp999@gmail.com>
2023-10-23 10:13:24 +0530

    added the file

--- File Upload and Download/controller/controller.js
+++ File Upload and Download/controller/controller.js
@@ -1,9 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
 const fileUploader = (req, res, next) => {
   res.render("index.ejs");
 };
 
 const uploadHandler = (req, res, next) => {
-    
+  const file = req.file;
   res.redirect("/");
 };
 
--- File Upload and Download/server.js
+++ File Upload and Download/server.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const multer=require("multer")
 
-
+const storageData=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null, 'folder')
+    },
+    filename:(req,file,cb)=>{
+        cb(null, new Date().toISOString()+'-'+file.originalname);
+    }
+})
 
 // routes
 const route = require("./routes/route");
@@ -11,6 +19,7 @@ const route = require("./routes/route");
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(multer({storage:storageData}).single('file'))
 app.use(route);
 
 app.listen(port, () => {
