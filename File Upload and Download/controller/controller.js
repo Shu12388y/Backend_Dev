b22b7afc90be7296aa3e61ebd94b111f5e618034
Shu12388y <spl.sp999@gmail.com>
2023-10-23 11:26:03 +0530

    file added

--- File Upload and Download/controller/controller.js
+++ File Upload and Download/controller/controller.js
@@ -14,8 +14,22 @@ const downloadHandler = (req, res, next) => {
   res.render("download.ejs");
 };
 
+const fileDownload = (req, res, next) => {
+  const filePath = path.join(__dirname, "../folder", "test.txt");
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.setHeader("Content-Type", "application/octet-stream");
+    res.setHeader("Content-Disposition", 'attachment; filename="test.txt"');
+    res.send(data);
+  });
+};
 module.exports = {
   fileUploader,
   uploadHandler,
   downloadHandler,
+  fileDownload,
 };
--- File Upload and Download/routes/route.js
+++ File Upload and Download/routes/route.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const route = express.Router();
 
-const { fileUploader, uploadHandler,downloadHandler } = require("../controller/controller");
+const {
+  fileUploader,
+  uploadHandler,
+  downloadHandler,
+  fileDownload,
+} = require("../controller/controller");
 
 route.get("/", fileUploader);
 
+route.post("/upload", uploadHandler);
 
+route.get("/download", downloadHandler);
 
-route.post("/upload",uploadHandler)
-
-route.get("/download",downloadHandler)
+route.get("/filedownload", fileDownload);
 
 module.exports = route;
