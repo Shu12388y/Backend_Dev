a26d7f86553d83d898bdd5a7a861ee88d63be3e0
Shu12388y <spl.sp999@gmail.com>
2024-03-20 16:39:40 +0530

    file added

---
+++ graphql/server.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv"
+dotenv.config({
+    path: '.env'
+});
+
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import pkg from "body-parser";
+import helmet from "helmet"
+import { createYoga } from 'graphql-yoga'
+
+
+const app = express()
+
+
+
+// configure middlewares
+const {urlencoded} = pkg
+app.use(express.json({limit:'16kb'}));
+app.use(cors({
+    origin:'http://localhost:5173'
+}));
+app.use(cookieParser());
+app.use(urlencoded({extended:'true',limit:'16kb'}));
+app.use(helmet())
+
+
+
+
+
+app.use("/",(_req,res)=>{
+    res.send("<h1>Hello World</h1>")
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port`,process.env.PORT);
+});
